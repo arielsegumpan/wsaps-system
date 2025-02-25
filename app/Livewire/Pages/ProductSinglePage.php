33c8b3cc66e1a3f264ed077a5851bcfc462b94e5
arielsegumpan <arielsegumpan31@gmail.com>
2025-02-25 20:18:47 +0800

    update product single page

--- app/Livewire/Pages/ProductSinglePage.php
+++ app/Livewire/Pages/ProductSinglePage.php
@@ -12,6 +12,7 @@ class ProductSinglePage extends Component
 
     public $prod_slug;
     public $prod_single;
+    public $related_products;
     public function mount($prod_slug)
     {
         $this->prod_single = Product::with([
@@ -23,14 +24,35 @@ class ProductSinglePage extends Component
         ])
         ->where('prod_slug', $prod_slug)
         ->first();
+
+        if ($this->prod_single) {
+            $this->getRelatedProducts();
+        }
+    }
+
+    private function getRelatedProducts()
+    {
+        // Get category IDs of the current product
+        $categoryIds = $this->prod_single->productCategories->pluck('id')->toArray();
+
+        $this->related_products = Product::with(['productImages', 'brand:id,brand_name'])
+            ->whereHas('productCategories', function ($query) use ($categoryIds) {
+                $query->whereIn('product_categories.id', $categoryIds); // Ensure correct table alias if needed
+            })
+            ->where('id', '!=', $this->prod_single->id) // Exclude current product
+            ->limit(6) // Get a limited number of related products
+            ->get();
     }
 
     #[Layout('layouts.app')]
     #[Title('Shop')]
     public function render()
     {
-        return view('livewire.pages.shop-page-single',[
-            'product' => $this->prod_single
+
+        // dd($this->related_products);
+        return view('livewire.pages.product-single-page',[
+            'product' => $this->prod_single,
+            'related_products' => $this->related_products
         ]);
     }
 }
